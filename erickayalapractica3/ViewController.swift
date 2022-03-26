b250876285c9d5b33efa58d4d7a8da2e4fdface0
Erick Ayala <ayala.delgadillo.erick@icloud.com>
2022-03-26 14:53:48 -0600

    Initial Commit

---
+++ erickayalapractica3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  erickayalapractica3
+//
+//  Created by Erick Ayala Delgadillo on 26/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
